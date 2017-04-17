513677257769ca00b4f31d18d843e5966682b174
Mike-LinuxPuter <mikewski333@gmail.com>
2017-04-17 15:47:07 -0700

    switched to using Mopidy javascript framework

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var session = require('express-session');
 var mopidy = require('./mopidyApi/api.js')
 
 var app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({secret: process.env.SESSION_SECRET}))
 
 app.post('/sms', function(req, res) {
     console.log(req.body.Body);
@@ -13,10 +15,10 @@ app.post('/sms', function(req, res) {
 });
 
 function searchResult(results){
-    mopidy.addToQueue(results.result[0].tracks[0].uri, function(response){
+    mopidy.addToQueue(results[0].tracks[0].uri, function(response){
         console.log(response);
     });
-    console.log("Queueing up: " + results.result[0].tracks[0].name + " - " + results.result[0].tracks[0].artists[0].name);
+    console.log("Queueing up: " + results[0].tracks[0].name + " - " + results[0].tracks[0].artists[0].name);
 }
 
 app.listen(3000, function(){
--- mopidyApi/api.js
+++ mopidyApi/api.js
@@ -1,58 +1,24 @@
 'use strict'
 
 var request = require('request');
+var Mopidy = require('mopidy');
+
+var mopidy = new Mopidy({
+    webSocketUrl: "ws://localhost:6680/mopidy/ws"
+});
 
 let mopidyHttpEnd = 'http://localhost:6680/mopidy/rpc';
 
 exports.searchMopidy = function (param, callback) {
-    request({
-            uri: mopidyHttpEnd,
-            method: 'POST',
-            'content-type': 'application/json',
-            body: {
-                "method": "core.library.search",
-                "jsonrpc": "2.0",
-                "params": {
-                    "query": {
-                        "any": [param]
-                },
-                "uris": ["spotify:"]
-                },
-                "id": 1
-            },
-            json: true
-        },
-
-        function (error, response, body) {
-            if(error){
-                console.log('searching failed');
-            }
-            if(callback)
-                callback(body);
+    mopidy.library.search({
+        "any": [param]}, ["spotify:"])
+        .then(function(data){
+            callback(data)
         });
 }
 
-exports.addToQueue = function(uri, callback){
-    request({
-        uri: mopidyHttpEnd,
-        method: 'POST',
-        'content-type': 'application/json',
-            body: {
-                "method": "core.tracklist.add",
-                "jsonrpc": "2.0",
-                "params": {
-                    "uri": uri
-                },
-                "id": 1
-            },
-            json: true
-        },
-
-        function (error, response, body) {
-            if(error){
-                console.log('add to queue failed');
-            }
-            if(callback)
-                callback(body);
-        });
+exports.addToQueue = function (uri, callback) {
+    mopidy.tracklist.add(null, null, uri, null).then(function(data){
+        callback(data);
+    })
 }
