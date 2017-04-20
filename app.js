d089d360c3861178dbe9ed5c0232617819989441
Mike-LinuxPuter <mikewski333@gmail.com>
2017-04-19 21:33:53 -0700

    Play, pause, next functionality implemented

--- app.js
+++ app.js
@@ -10,63 +10,110 @@ var twilio = require('twilio');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({secret: config.SESSION_SECRET}));
+var actions = {
+    play: {
+        text: "$play",
+        action: function(twiml, res){
+            let message = "Playing..."
+            mopidy.playQueue();
+            sendResponse(twiml, res, message);
+        }
+    },
+    pause: {
+        text: "$pause",
+        action: function(twiml, res){
+            let message = "Pausing...";
+            mopidy.pauseQueue();
+            sendResponse(twiml, res, message);
+        }
+    },
+    next: {
+        text: "$next",
+        action: function(twiml, res){
+            let message = "Playing next...";
+            mopidy.playNextTrack();
+            sendResponse(twiml, res, message);
+        }
+    },
+}
 
-app.post('/sms', function(req, res) {
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(session({
+    secret: config.SESSION_SECRET
+}));
+
+app.post('/sms', function (req, res) {
     console.log(req.body.Body);
     let textBod = req.body.Body;
     let phoneNum = req.body.From;
 
-    if(!req.session.users){
+    if (!req.session.users) {
         req.session.users = {};
     }
     var songs = req.session.users[phoneNum];
-    
+
     var twiml = new twilio.TwimlResponse();
     var message = "";
-    if(songs){
-        if(!isNaN(textBod) && textBod >= 0 && textBod < songs.length){
-            message = "Queueing up: " + songs[textBod].title + " - " + songs[textBod].artist;
-            mopidy.addToQueue(songs[textBod].uri);
-            delete req.session.users[phoneNum];
-        } else {
-            message = "Don't be an ass, just make a song choice";
-        }
-        sendResponse(twiml, res, message);
+    if(isAction(textBod)){
+        performAction(twiml, res, textBod);
+    } else if (songs) {
+        makeSelection(textBod, phoneNum, twiml, req, res, message, songs);
     } else {
-        req.session.users[phoneNum] = [];
-        mopidy.searchMopidy(req.body.Body).then(function(results){
-            message = "Respond with song choice\n";
-            let tracks = results[0].tracks;
-            let limit = tracks.length < 3 ? tracks.length : 3;
-            for(var i = 0; i < limit; i++){
-                message += i + ": " + tracks[i].name + " - " + tracks[i].artists[0].name + "\n";
-                req.session.users[phoneNum].push({
-                    "uri": tracks[i].uri,
-                    "title": tracks[i].name,
-                    "artist": tracks[i].artists[0].name
-                });
-            }
-            sendResponse(twiml, res, message);
-        });
+        search(textBod, phoneNum, twiml, req, res, message);
     }
 });
 
-function sendResponse(twiml, res, message){
+function isAction(textBod){
+    return textBod == actions.play.text || textBod == actions.pause.text || textBod == actions.next.text;
+}
+
+function performAction(twiml, res, textBod){
+    let playbackAction = textBod.toLowerCase().substring(1);
+    actions[playbackAction].action(twiml, res);
+}
+
+function sendResponse(twiml, res, message) {
     twiml.message(message);
-    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.writeHead(200, {
+        'Content-Type': 'text/xml'
+    });
     res.end(twiml.toString());
 }
 
-// function searchResult(results, phoneNum){
+function makeSelection(textBod, phoneNum, twiml, req, res, message, songs) {
+    if(textBod == "!"){
+        message = "Oh shit you don canceled it!";
+        delete req.session.users[phoneNum];
+    } else if (!isNaN(textBod) && textBod >= 0 && textBod < songs.length) {
+        message = "Queueing up: " + songs[textBod].title + " - " + songs[textBod].artist;
+        mopidy.addToQueue(songs[textBod].uri, true);
+        delete req.session.users[phoneNum];
+    } else {
+        message = "Don't be an ass, just make a song choice...or '!' to cancel";
+    }
+    sendResponse(twiml, res, message);
+}
 
-//     mopidy.addToQueue(results[0].tracks[0].uri, function(response){
-//         console.log(response);
-//     });
-//     console.log("Queueing up: " + results[0].tracks[0].name + " - " + results[0].tracks[0].artists[0].name);
-// }
+function search(textBod, phoneNum, twiml, req, res, message) {
+    req.session.users[phoneNum] = [];
+    mopidy.searchMopidy(textBod).then(function (results) {
+        message = "Respond with song choice or '!' to cancel\n";
+        let tracks = results[0].tracks;
+        let limit = tracks.length < 3 ? tracks.length : 3;
+        for (var i = 0; i < limit; i++) {
+            message += i + ": " + tracks[i].name + " - " + tracks[i].artists[0].name + "\n";
+            req.session.users[phoneNum].push({
+                "uri": tracks[i].uri,
+                "title": tracks[i].name,
+                "artist": tracks[i].artists[0].name
+            });
+        }
+        sendResponse(twiml, res, message);
+    });
+}
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Listenting on port 3000');
 })
--- mopidyApi/api.js
+++ mopidyApi/api.js
@@ -14,8 +14,41 @@ exports.searchMopidy = function (param, callback, phoneNum) {
         "any": [param]}, ["spotify:"]);
 }
 
-exports.addToQueue = function (uri, callback) {
+exports.addToQueue = function (uri, autoplay) {
     mopidy.tracklist.add(null, null, uri, null).then(function(data){
+        console.log("Queueing up: " + data[0].track.name + " - " + data[0].track.artists[0].name);
+        let tlid = data[0];
+        if(autoplay){
+            mopidy.playback.getState().then(function(data){
+                console.log(data);
+                if(data === 'stopped'){
+                    mopidy.playback.play(tlid).then(function(data){
+                        console.log("Playing...");
+                    });
+                }
+                // console.log(data);
+            });
+        }
+    });
+}
+
+exports.playNextTrack = function(){
+    mopidy.playback.next().then(function(data){
         console.log(data);
+        mopidy.playback.play().then(function(data){
+            console.log("Next...");
+        });
+    });
+}
+
+exports.pauseQueue = function(){
+    mopidy.playback.pause().then(function(data){
+        console.log("Pausing...");
+    });
+}
+
+exports.playQueue = function(){
+    mopidy.playback.play().then(function(data){
+        console.log("Playing...");
     })
 }
